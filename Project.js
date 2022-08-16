52337b65cb127982cc7a670346dda151b3ffb4f8
Alexander <alexlib1995@gmail.com>
2022-08-16 17:30:03 +0300

    add questions

--- Project.js
+++ Project.js
@@ -1,1 +1,14 @@
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+let A = prompt("Один из последних просмотренных фильмов?"),
+    B = prompt("На сколько оцените его?"),
+    C = prompt("Один из последних просмотренных фильмов?"),
+    D = prompt("На сколько оцените его?");
