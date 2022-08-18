983504d350d79b3729f240575e3ab31ab2e3eaf9
Alexander <alexlib1995@gmail.com>
2022-08-18 19:32:15 +0300

    add terms

--- Project.js
+++ Project.js
@@ -1,4 +1,6 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+'use strict'
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
 count: numberOfFilms,
@@ -8,7 +10,31 @@ genres: [],
 privat: false
 };
 
-let A = prompt("Один из последних просмотренных фильмов?"),
-    B = prompt("На сколько оцените его?"),
-    C = prompt("Один из последних просмотренных фильмов?"),
-    D = prompt("На сколько оцените его?");
+
+
+for (let i = 0; i < 2; i++){
+    const A = prompt("Один из последних просмотренных фильмов?", ""),
+          B = prompt("На сколько оцените его?", "");
+
+    if (A != null && B !=null && A != "" && B != "" && A.length < 50) {
+        personalMovieDB.movies[A] = B;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+alert("Просмотрено довольно мало фильмов", "")
+}else if (personalMovieDB.count < 30){
+alert("Вы классический зритель", "")
+}else if (personalMovieDB.count >= 30){
+alert("Вы киноман", "")
+}else {
+alert("Произошла ошибка", "")
+}
+
+
+
+console.log(personalMovieDB);
